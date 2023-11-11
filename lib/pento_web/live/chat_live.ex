68c81b9680f4380604c020f109f40342e782c749
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2023-11-11 12:44:31 +0200

    Add image upload

--- lib/pento_web/live/chat_live.ex
+++ lib/pento_web/live/chat_live.ex
@@ -24,7 +24,7 @@ defmodule PentoWeb.ChatLive do
         class="flex-1 p:2 sm:p-6 justify-between flex flex-col-reverse overflow-y-auto scrolling-auto h-96"
       >
         <.table id="messages" rows={@streams.messages}>
-          <:col :let={{_id, message}} label="User" class="w-8"><%= message.user %></:col>
+          <:col :let={{_id, message}} label="User"><%= message.user %></:col>
           <:col :let={{_id, message}} label="Time"><%= message.inserted_at %></:col>
           <:col :let={{_id, message}} label="Message"><%= message.text %></:col>
         </.table>
@@ -58,6 +58,6 @@ defmodule PentoWeb.ChatLive do
   end
 
   def time() do
-    Time.utc_now()|> Time.truncate(:second) |> to_string()
+    Time.utc_now() |> Time.truncate(:second) |> to_string()
   end
 end
